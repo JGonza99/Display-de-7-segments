1b49b87c7fff42cf2cba5ba96c03d7964868da57
JGonza99 <JGonza99@users.noreply.github.com>
2019-02-18 17:34:52 +0100

    Primera pujada

--- Programa01/Programa01.ino
+++ Programa01/Programa01.ino
@@ -8,6 +8,7 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
+const int Pols = 4;       // donar nom al pin 4 de l’Arduino
 const int A = 5;          // donar nom al pin 5 de l’Arduino
 const int B = 6;          // donar nom al pin 6 de l’Arduino
 const int C = 7;          // donar nom al pin 7 de l’Arduino
@@ -15,12 +16,13 @@ const int D = 8;          // donar nom al pin 8 de l’Arduino
 const int E = 9;          // donar nom al pin 9 de l’Arduino
 const int F = 10;         // donar nom al pin 10 de l’Arduino
 const int G = 11;         // donar nom al pin 11 de l’Arduino
-const int Dot = 12;         // donar nom al pin 12 de l’Arduino
+const int Dot = 12;       // donar nom al pin 12 de l’Arduino
 unsigned long a;
 
 //********** Setup ****************************************************************
 void setup()
 {
+  pinMode(Pols,INPUT);    // definir el pin 4 com una entrada
   pinMode(A, OUTPUT);     // definir el pin 5 com una sortida
   pinMode(B, OUTPUT);     // definir el pin 6 com una sortida
   pinMode(C, OUTPUT);     // definir el pin 7 com una sortida
@@ -35,113 +37,130 @@ void setup()
 //********** Loop *****************************************************************
 void loop()
 {
-  digitalWrite(A, HIGH);    
-  digitalWrite(B, HIGH);    
-  digitalWrite(C, HIGH);     
-  digitalWrite(D, HIGH);    //Num 0
-  digitalWrite(E, HIGH);    
-  digitalWrite(F, HIGH);   
-  digitalWrite(G, LOW);
-  digitalWrite(Dot, HIGH);  
+  do 
+  {
+      digitalWrite(A, HIGH);    
+      digitalWrite(B, HIGH);    
+      digitalWrite(C, HIGH);     
+      digitalWrite(D, HIGH);    //Num 0
+      digitalWrite(E, HIGH);    
+      digitalWrite(F, HIGH);   
+      digitalWrite(G, LOW);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+    
+      digitalWrite(A, LOW);    
+      digitalWrite(B, HIGH);    
+      digitalWrite(C, HIGH);     
+      digitalWrite(D, LOW);     //Num 1
+      digitalWrite(E, LOW);    
+      digitalWrite(F, LOW);   
+      digitalWrite(G, LOW);
+      digitalWrite(Dot, HIGH);    
+    
+      delay(a);
+    
+      digitalWrite(A, HIGH);    
+      digitalWrite(B, HIGH);    
+      digitalWrite(C, LOW);      //Num 2
+      digitalWrite(D, HIGH);    
+      digitalWrite(E, HIGH);    
+      digitalWrite(F, LOW);   
+      digitalWrite(G, HIGH);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+    
+      digitalWrite(A, HIGH);    
+      digitalWrite(B, HIGH);    
+      digitalWrite(C, HIGH);      //Num 3
+      digitalWrite(D, HIGH);    
+      digitalWrite(E, LOW);    
+      digitalWrite(F, LOW);   
+      digitalWrite(G, HIGH);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+    
+      digitalWrite(A, LOW);    
+      digitalWrite(B, HIGH);    
+      digitalWrite(C, HIGH);      //Num 4
+      digitalWrite(D, LOW);    
+      digitalWrite(E, LOW);    
+      digitalWrite(F, HIGH);   
+      digitalWrite(G, HIGH);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+    
+      digitalWrite(A, HIGH);    
+      digitalWrite(B, LOW);    
+      digitalWrite(C, HIGH);      //Num 5
+      digitalWrite(D, HIGH);    
+      digitalWrite(E, LOW);    
+      digitalWrite(F, HIGH);   
+      digitalWrite(G, HIGH);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+    
+      digitalWrite(A, LOW);    
+      digitalWrite(B, LOW);    
+      digitalWrite(C, HIGH);      //Num 6
+      digitalWrite(D, HIGH);    
+      digitalWrite(E, HIGH);    
+      digitalWrite(F, HIGH);   
+      digitalWrite(G, HIGH);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+    
+      digitalWrite(A, HIGH);    
+      digitalWrite(B, HIGH);    
+      digitalWrite(C, HIGH);      //Num 7
+      digitalWrite(D, LOW);    
+      digitalWrite(E, LOW);    
+      digitalWrite(F, LOW);   
+      digitalWrite(G, LOW);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+    
+      digitalWrite(A, HIGH);    
+      digitalWrite(B, HIGH);    
+      digitalWrite(C, HIGH);      //Num 8
+      digitalWrite(D, HIGH);    
+      digitalWrite(E, HIGH);    
+      digitalWrite(F, HIGH);   
+      digitalWrite(G, HIGH);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+    
+      digitalWrite(A, HIGH);    
+      digitalWrite(B, HIGH);    
+      digitalWrite(C, HIGH);      //Num 9
+      digitalWrite(D, LOW);    
+      digitalWrite(E, LOW);    
+      digitalWrite(F, HIGH);   
+      digitalWrite(G, HIGH);
+      digitalWrite(Dot, HIGH);  
+      
+      delay(a);
+  }
+  while(digitalRead(Pols) == LOW);
   
-  delay(a);
-
-  digitalWrite(A, LOW);    
-  digitalWrite(B, HIGH);    
-  digitalWrite(C, HIGH);     
-  digitalWrite(D, LOW);     //Num 1
-  digitalWrite(E, LOW);    
-  digitalWrite(F, LOW);   
-  digitalWrite(G, LOW);
-  digitalWrite(Dot, HIGH);    
-
-  delay(a);
-
-  digitalWrite(A, HIGH);    
-  digitalWrite(B, HIGH);    
-  digitalWrite(C, LOW);      //Num 2
-  digitalWrite(D, HIGH);    
-  digitalWrite(E, HIGH);    
-  digitalWrite(F, LOW);   
-  digitalWrite(G, HIGH);
-  digitalWrite(Dot, HIGH);  
-  
-  delay(a);
-
-  digitalWrite(A, HIGH);    
-  digitalWrite(B, HIGH);    
-  digitalWrite(C, HIGH);      //Num 3
-  digitalWrite(D, HIGH);    
-  digitalWrite(E, LOW);    
-  digitalWrite(F, LOW);   
-  digitalWrite(G, HIGH);
-  digitalWrite(Dot, HIGH);  
-  
-  delay(a);
-
-  digitalWrite(A, LOW);    
-  digitalWrite(B, HIGH);    
-  digitalWrite(C, HIGH);      //Num 4
-  digitalWrite(D, LOW);    
-  digitalWrite(E, LOW);    
-  digitalWrite(F, HIGH);   
-  digitalWrite(G, HIGH);
-  digitalWrite(Dot, HIGH);  
-  
-  delay(a);
-
-  digitalWrite(A, HIGH);    
-  digitalWrite(B, LOW);    
-  digitalWrite(C, HIGH);      //Num 5
-  digitalWrite(D, HIGH);    
-  digitalWrite(E, LOW);    
-  digitalWrite(F, HIGH);   
-  digitalWrite(G, HIGH);
-  digitalWrite(Dot, HIGH);  
-  
-  delay(a);
-
-  digitalWrite(A, LOW);    
-  digitalWrite(B, LOW);    
-  digitalWrite(C, HIGH);      //Num 6
-  digitalWrite(D, HIGH);    
-  digitalWrite(E, HIGH);    
-  digitalWrite(F, HIGH);   
-  digitalWrite(G, HIGH);
-  digitalWrite(Dot, HIGH);  
-  
-  delay(a);
-
-  digitalWrite(A, HIGH);    
-  digitalWrite(B, HIGH);    
-  digitalWrite(C, HIGH);      //Num 7
-  digitalWrite(D, LOW);    
-  digitalWrite(E, LOW);    
-  digitalWrite(F, LOW);   
-  digitalWrite(G, LOW);
-  digitalWrite(Dot, HIGH);  
-  
-  delay(a);
-
-  digitalWrite(A, HIGH);    
-  digitalWrite(B, HIGH);    
-  digitalWrite(C, HIGH);      //Num 8
-  digitalWrite(D, HIGH);    
-  digitalWrite(E, HIGH);    
-  digitalWrite(F, HIGH);   
-  digitalWrite(G, HIGH);
-  digitalWrite(Dot, HIGH);  
-  
-  delay(a);
-
-  digitalWrite(A, HIGH);    
-  digitalWrite(B, HIGH);    
-  digitalWrite(C, HIGH);      //Num 9
-  digitalWrite(D, LOW);    
-  digitalWrite(E, LOW);    
-  digitalWrite(F, HIGH);   
-  digitalWrite(G, HIGH);
-  digitalWrite(Dot, HIGH);  
-  
-  delay(a);
+   if(digitalRead(Pols) == HIGH);
+  {
+    digitalWrite(A, LOW);    
+      digitalWrite(B, LOW);    
+      digitalWrite(C, LOW);      
+      digitalWrite(D, LOW);    
+      digitalWrite(E, LOW);    
+      digitalWrite(F, LOW);   
+      digitalWrite(G, LOW);
+      digitalWrite(Dot, LOW);
+      while(digitalRead(Pols) == HIGH);
+  }
 }
